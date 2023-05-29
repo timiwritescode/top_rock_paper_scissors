450c9165846663f1351d4934959aafd85ff0ecec
Oluwatimilehin <odebodezion@gmail.com>
2023-05-29 21:32:35 +0100

    Configure UI with functions

--- script/script.js
+++ script/script.js
@@ -1,6 +1,10 @@
 // let's rock paper scissors yo!
+const playerChoice = document.querySelector('.player-panel').children;
 
-// get the randomIndex since js annoyingly does not have a randome module
+console.log(playerChoice)
+
+
+// get the randomIndex since js annoyingly does not have a random module
 // annoyingly
 function getRandomIndex() {
     // function to get the random index from 0 to 2
@@ -15,15 +19,59 @@ function getRandomIndex() {
 // get computer's choice. Rock paper or scissors
 function getComputerChoice () {
     let choices = 'rps';
+    
     return choices[getRandomIndex()];
 };
 
+function unPresentComputerWeapon() {
+    let weaponContainer = document.querySelector('#computer-choice');
+    console.log(weaponContainer)
+    if (weaponContainer.children[0]){
+    weaponContainer.removeChild(weaponContainer.children[0])
+    };
+};
+
+function presentComputerWeapon(computerWeapon) {
+    let computerPanel = document.querySelector('.computer-panel');
+    Array.from(computerPanel.children).forEach((f) => {
+        if (f.id === computerWeapon) {
+            presentWeapon(f, '#computer-choice')
+        }
+    });
+
+}
+
+function presentWeapon(weapon, classname) {
+    let container = document.querySelector(classname);
+    let weaponChoice = weapon.children[0].cloneNode(true);
+    weaponChoice.className = 'choice-img';
+    if (container.children[0] === undefined) {
+        
+        container.appendChild(weaponChoice)
+        
+    } else {
+        
+        container.replaceChild(weaponChoice, container.children[0])
+        
+    };
+    
+};
+
 // get the user's choice
-const usrChoice = document.querySelector('input');
+let usrChoice ;
+
+Array.from(playerChoice).forEach((choice) => {
+        choice.addEventListener('click', () => {
+            usrChoice = choice.id;
+            presentWeapon(choice, '#player-choice');
+            unPresentComputerWeapon()    
+        }) 
+});
+
 const submitBtn = document.querySelector('button');
 let computerChoice = getComputerChoice()
 
-submitBtn.addEventListener('click', () => {getWinner(usrChoice.value, computerChoice)})    
+submitBtn.addEventListener('click', () => {getWinner(usrChoice, computerChoice)})    
 
 // put both choices against each other and decide based on rule
 function declareWinner (player, computer) {
@@ -49,6 +97,8 @@ function declareWinner (player, computer) {
 let playerScore = 0;
 let computerScore = 0;
 
+let uiPlayerScore = document.querySelector('.player-score')
+let uiComputerScore = document.querySelector('.computer-score')
 
 // Utility functions go here
 function translateAbbr (abbr) {
@@ -69,16 +119,22 @@ function translateAbbr (abbr) {
 function isRoundComplete (player, computer) {
     // This function check if the player and computer scores add up to five
     // to declare a round complete or not.
-    return player + computer === 5 ? true : false
-}
+    if (player === 3 || computer === 3) {
+        return true
+    };
+    return false;
+};
 
 function resetGame () {
     // This function reset game when called but reasonably after
     //      best out of a playerScore + gameScore = five
     playerScore = 0;
-    computerChoice = 0;
+    computerScore = 0;
+    uiComputerScore.textContent = computerScore
+    uiPlayerScore.textContent = playerScore
     console.clear()
     console.log('Game Restarted')
+
 }
 
 // The game engine itself
@@ -86,12 +142,7 @@ function getWinner (userChoice, computerChoice) {
     console.log(`Your choice: ${translateAbbr(userChoice)}`);
     console.log(`Computer: ${translateAbbr(computerChoice)}`);
 
-    // reload computer choice
-    computerChoice = getComputerChoice()
-
-    let winner = declareWinner(userChoice, computerChoice);
-    
-
+    // Test the game state
     switch (isRoundComplete(playerScore, computerScore)) {
         case true:
             console.log(`Final score: Player ${playerScore} - ${computerScore} Computer`)
@@ -102,17 +153,23 @@ function getWinner (userChoice, computerChoice) {
 
         default:
             // console.clear()
+            // reload computer choice
+            computerChoice = getComputerChoice()
+            presentComputerWeapon(computerChoice)
+            let winner = declareWinner(userChoice, computerChoice);
             switch(winner) {
                 case userChoice:
                     console.log(`${translateAbbr(userChoice)} beats ${translateAbbr(computerChoice)}`)
                     console.log('You win!')
                     ++playerScore 
+                    
                     break;
         
                 case computerChoice:
                     console.log(`${translateAbbr(computerChoice)} beats ${translateAbbr(userChoice)}`)
                     console.log('You lose!')
                     ++computerScore
+                    
                     break;
         
                 case 'Draw':
@@ -120,7 +177,14 @@ function getWinner (userChoice, computerChoice) {
                     console.log('Tie!')  
                 };
                 console.log(`Player ${playerScore} - ${computerScore} Computer`)
-        };               
+        };
+
+    
+    
+
+    
+        uiComputerScore.textContent = computerScore
+        uiPlayerScore.textContent = playerScore               
     };
 
     
