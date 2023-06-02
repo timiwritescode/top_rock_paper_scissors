d9136b20663f2687233eba3144f9a512044912c2
Oluwatimilehin <odebodezion@gmail.com>
2023-06-02 21:45:27 +0100

    Add events to nav buttons

--- script/script2.js
+++ script/script2.js
@@ -69,10 +69,45 @@ startInterval()
 
 function toggleInstructions () {
     let navButton = document.querySelector('.toggle-pop-up');
+    let instructionBtn = document.querySelector('.instruction-header');
+    let rulesBtn = document.querySelector('.rules-header');
     navButton.addEventListener('click', () => {
         let popUp = document.querySelector('.blur-bg');
         popUp.style.display = 'flex';
-    })
+    });
+
+    instructionBtn.addEventListener('click', () => {
+        let popUp = document.querySelector('.blur-bg');
+        let welcomeMsg = document.querySelector('.welcome');
+        let instruction = document.querySelector('.instruction');
+        let rules = document.querySelector('.rules');
+        let rulesBtn = document.querySelector('.rules-btn');
+        let instructionBtn = document.querySelector('.instruction-btn')
+        welcomeMsg.style.display = 'none';
+        popUp.style.display = 'flex';
+        instruction.style.display = 'inherit';
+        rulesBtn.style.display = 'none';
+        instructionBtn.style.display = 'none';
+        rules.style.display = 'none';
+        
+    });
+
+    rulesBtn.addEventListener('click', () => {
+        let popUp = document.querySelector('.blur-bg');
+        let welcomeMsg = document.querySelector('.welcome');
+        let rules = document.querySelector('.rules');
+        let instruction = document.querySelector('.instruction');
+        let rulesBtn = document.querySelector('.rules-btn');
+        let instructionBtn = document.querySelector('.instruction-btn')
+        welcomeMsg.style.display = 'none';
+        popUp.style.display = 'flex';
+        rules.style.display = 'inherit';
+        instruction.style.display = 'none';
+        rulesBtn.style.display = 'none';
+        instructionBtn.style.display = 'none'
+        
+    });
+
 };
 
 toggleInstructions()
